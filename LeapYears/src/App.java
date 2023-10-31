6dcee9af08efac57c5d0f47d9f889e9b21a8638f
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2023-10-30 22:02:02 -0300

    Adicionando novo exercício Merge Arrays

--- LeapYears/src/App.java
+++ LeapYears/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(LeapYears.isLeapYear(2020));
+        System.out.println(LeapYears.isLeapYear(2013));
     }
 }
